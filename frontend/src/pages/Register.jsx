7d74807f62a734f61059108591e0db8cd11b6824
mac stephens <macstephens@gmail.com>
2023-09-07 11:22:22 -0400

    Working on frontend auth

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import {FaUser} from 'react-icons/fa'
+
+function Register() {
+const [formData, setFormData] = useState({
+  name: '',
+  email: '',
+  password: '',
+  password2: ''
+})
+
+const { name, email, password, password2 } = formData;
+
+
+
+  return <>
+    <section className="heading">
+      <h1>
+        <FaUser /> Register
+      </h1>
+      <p>Please create an account</p>
+    </section>
+
+    <section className="form">
+      <form>
+        input.
+      </form>
+    </section>
+  </>;
+}
+
+export default Register;
