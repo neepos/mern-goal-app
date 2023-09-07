8f8ff9ad5d14455b49ee377c9a2cb24f0adc0e5f
mac stephens <macstephens@gmail.com>
2023-09-07 11:46:15 -0400

    Added frontend UI

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,9 +12,21 @@ const [formData, setFormData] = useState({
 
 const { name, email, password, password2 } = formData;
 
+const onChange = (e) => {
+  setFormData((prevState) => ({
+    ...prevState,
+    [e.target.name]: e.target.value,
+  }))
+};
+
+const onSubmit = (e) => {
+  e.preventDefault()
+}
 
 
-  return <>
+
+  return ( 
+  <>
     <section className="heading">
       <h1>
         <FaUser /> Register
@@ -23,11 +35,58 @@ const { name, email, password, password2 } = formData;
     </section>
 
     <section className="form">
-      <form>
-        input.
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <input 
+          type="text" 
+          className="form-control" 
+          id="name" 
+          name="name" 
+          value={name}
+          placeholder="Enter your name"
+          onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input 
+          type="email" 
+          className="form-control" 
+          id="email" 
+          name="email" 
+          value={email}
+          placeholder="Enter your email"
+          onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input 
+          type="password" 
+          className="form-control" 
+          id="password" 
+          name="password" 
+          value={password}
+          placeholder="Enter password"
+          onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <input 
+          type="password" 
+          className="form-control" 
+          id="password2" 
+          name="password2" 
+          value={password2}
+          placeholder="Confirm password"
+          onChange={onChange}
+          />
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-block">Submit</button>
+        </div>
       </form>
     </section>
-  </>;
+  </>
+  )
 }
 
 export default Register;
