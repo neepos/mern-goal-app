8426ad3217b5faf224c3c2e735e81dd4a73e0b64
dan barker <dbarker16@gmail.com>
2023-09-08 15:46:42 -0400

    final product before deployment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -80,12 +80,7 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route GET /api/users/me
 // @access Private
 const getMe = asyncHandler(async (req, res) => {
-    const {_id, name, email} = await User.findById(req.user.id).select('-password')
-    res.status(200).json({
-        id: _id,
-        name,
-        email,
-    })
+    res.status(200).json(req.user);
 });
 
 // Generate a JWTtoken
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,10 +5,10 @@ import { useSelector, useDispatch } from 'react-redux'
 import { logout, reset } from '../features/auth/authSlice'
 
 function Header() {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const {user} = useSelector(state => state.auth)
-    
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {user} = useSelector((state) => state.auth)
+
     const onLogout = () => {
         dispatch(logout())
         dispatch(reset())
